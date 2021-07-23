d50aa9ae55b5edb3f35b63e143176d19b34510a9
GueldenBayar <g.bayar@hotmail.de>
2021-07-23 16:51:37 +0200

    routing added

---
+++ src/Components/SidebarComponents/AlienDebugger.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AlienDebugger() {
+    return (
+        <div>
+            <h1>Alien Debugger</h1>
+        </div>
+    );
+}
+
+export default AlienDebugger;
