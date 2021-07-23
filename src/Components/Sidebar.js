ebe0175b30fbbc4fc3e99b7ba2b4177d7595d79c
zinaelnahel <zinamahmoud1989@gmail.com>
2021-07-23 10:24:42 +0200

    add sidebar

--- src/Components/Sidebar.js
+++
@@ -1,5 +0,0 @@
-// https://material-ui.com/components/drawers/
-// use Audiowave font (linked in sidebar.css)
-// look for icons and credit where they r taken from in the credits on the bottom
-// create routes for each component in the sidebar
-// styling should also be relevant to the projec design
