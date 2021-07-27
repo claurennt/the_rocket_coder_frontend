e8551fea88744e43e3b8801b8595d458f56455f1
zinaelnahel <zinamahmoud1989@gmail.com>
2021-07-27 16:26:52 +0200

    add quiz

--- src/Components/RoboCodingQuiz.js
+++ src/Components/RoboCodingQuiz.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React from "react";
+import Quiz from "react-quiz-component";
+import { quiz } from "./quiz";
+import Robot from "./Robot.jpeg";
 
 function RoboCodingQuiz() {
-    return (
-        <div>
-            <h1>RoboCodingQuiz</h1>
-        </div>
-    );
+  const onCompleteAction = (obj) => {
+    console.log(obj);
+    // YOUR LOGIC GOES HERE
+  };
+  return (
+    <div
+      className="background"
+      style={{
+        background: ` url(${Robot}`,
+        backgroundPosition: "top",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        height: "100vh",
+      }}
+    >
+      <div
+        className="quizes"
+        style={{
+          marginLeft: "180px",
+          marginTop: "0px",
+        }}
+      >
+        <Quiz
+          quiz={quiz}
+          shuffle={true}
+          showDefaultResult={true}
+          onComplete={onCompleteAction}
+          showInstantFeedback={true}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default RoboCodingQuiz;
