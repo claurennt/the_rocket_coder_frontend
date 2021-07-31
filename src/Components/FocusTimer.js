aa6973cea476bd339127603ffe22d687b8839e36
zinaelnahel <zinamahmoud1989@gmail.com>
2021-07-31 20:34:33 +0200

    solve conflict 2

--- src/Components/FocusTimer.js
+++ src/Components/FocusTimer.js
@@ -1,80 +1,5 @@
 import React from "react";
 import ReactGlobe from "react-globe";
-<<<<<<< HEAD
-import * as THREE from "three";
-import { useState, useEffect } from "react";
-function FocusTimer({ weatherData }) {
-  const markers = [
-    {
-      id: "marker1",
-      color: "red",
-      coordinates: [weatherData.latitude, weatherData.longitude],
-      value: 50,
-    },
-  ];
-  const [animationSequence, setAnimationSequence] = useState([]);
-  console.log(weatherData.latitude, weatherData.longitude);
-  useEffect(() => {
-    setAnimationSequence([
-      {
-        coordinates: [weatherData.latitude, weatherData.longitude],
-        focusAnimationDuration: 3000,
-        focusDistanceRadiusScale: 3,
-        focusEasingFunction: ["Linear", "None"],
-      },
-    ]);
-  }, [weatherData.latitude, weatherData.longitude]);
-
-  // switch (animationSequence) {
-  //   case "singapore":
-  //     animations = [
-
-  //     ];
-  //     break;
-  //   case "world":
-  //     animations = [
-  //       {
-  //         coordinates: [1.3521, 103.8198],
-  //         focusAnimationDuration: 3000,
-  //         focusDistanceRadiusScale: 2,
-  //         focusEasingFunction: ["Linear", "None"],
-  //       },
-  //       {
-  //         coordinates: [39.9042, 116.4074],
-  //         focusAnimationDuration: 3000,
-  //         focusDistanceRadiusScale: 2,
-  //         focusEasingFunction: ["Cubic", "InOut"],
-  //       },
-  //       {
-  //         coordinates: [37.773972, -122.431297],
-  //         focusAnimationDuration: 3000,
-  //         focusDistanceRadiusScale: 2,
-  //         focusEasingFunction: ["Back", "InOut"],
-  //       },
-  //       {
-  //         coordinates: [40.73061, -73.935242],
-  //         focusAnimationDuration: 3000,
-  //         focusDistanceRadiusScale: 2,
-  //         focusEasingFunction: ["Circular", "InOut"],
-  //       },
-  //       {
-  //         coordinates: [51.5074, 0.1278],
-  //         focusAnimationDuration: 3000,
-  //         focusDistanceRadiusScale: 2,
-  //         focusEasingFunction: ["Quadratic", "InOut"],
-  //       },
-  //       {
-  //         coordinates: [1.3521, 103.8198],
-  //         focusAnimationDuration: 5000,
-  //         focusDistanceRadiusScale: 4,
-  //         focusEasingFunction: ["Linear", "None"],
-  //       },
-  //     ];
-  //     break;
-  //   default:
-  //     animations = [];
-  // }
-=======
 // import * as THREE from "three";
 import { useState, useEffect } from "react";
 function FocusTimer({ weatherData, latitude, longitude }) {
@@ -107,7 +32,6 @@ function FocusTimer({ weatherData, latitude, longitude }) {
     ]);
   }, [latitude, longitude]);
 
->>>>>>> 3d6a27d09706352415b017b12def22b1547bedd1
   return (
     <>
       {animationSequence && (
@@ -120,15 +44,7 @@ function FocusTimer({ weatherData, latitude, longitude }) {
             enableGlobeGlow: true,
             globeGlowCoefficient: 0.1,
             globeGlowColor: "gold",
-<<<<<<< HEAD
-            // globeGlowPower: 4,
-            // globeGlowRadiusScale: 0.5,
             cameraAutoRotateSpeed: 0.5,
-            // cameraMaxPolarAngle: (Math.PI * 9) / 16,
-            // cameraMinPolarAngle: (Math.PI * 7) / 16,
-=======
-            cameraAutoRotateSpeed: 0.5,
->>>>>>> 3d6a27d09706352415b017b12def22b1547bedd1
             cameraRotateSpeed: 1,
             enableCameraAutoRotate: true,
             enableCameraRotate: true,
@@ -147,10 +63,7 @@ function FocusTimer({ weatherData, latitude, longitude }) {
         <button onClick={() => setAnimationSequence("world")}>
           Travel the world
         </button> */}
-<<<<<<< HEAD
-=======
         <h1>12</h1>
->>>>>>> 3d6a27d09706352415b017b12def22b1547bedd1
       </div>
     </>
   );
