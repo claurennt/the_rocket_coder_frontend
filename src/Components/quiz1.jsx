7ad05c926719a868a0f3edb10a0a00126063ac5a
claurennt <c.benedetti1989@gmail.com>
2021-07-31 17:58:56 +0200

    add play again button in roboquiz

--- src/Components/quiz1.jsx
+++ src/Components/quiz1.jsx
@@ -1,12 +1,12 @@
 export const quiz1 = {
-  quizTitle: "Welcome to the RoboQuiz",
-  quizSynopsis:
-    "This quiz is designed for you to refresh your coding skills and help you on your journey to becoming a Rocket Coder 🚀",
+  // quizTitle: "Welcome to the RoboQuiz",
+  // quizSynopsis:
+  //   "This quiz is designed for you to refresh your coding skills and help you on your journey to becoming a Rocket Coder 🚀",
   questions: [
     {
       question:
         "How can you access the state of a component from inside of a member function?",
-      questionType: "text",// if you need to display Picture in Question
+      questionType: "text", // if you need to display Picture in Question
       answerSelectionType: "single",
       answers: [
         "this.getState()",
@@ -78,18 +78,12 @@ export const quiz1 = {
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
       point: "20",
     },
-      {
-      question:
-        "Arbitrary inputs of components are called __________.",
+    {
+      question: "Arbitrary inputs of components are called __________.",
       questionType: "text",
-       // if you need to display Picture in Question
+      // if you need to display Picture in Question
       answerSelectionType: "single",
-      answers: [
-        "Keys",
-        "Props",
-        "Elements",
-        "Ref",
-      ],
+      answers: ["Keys", "Props", "Elements", "Ref"],
       correctAnswer: "2",
       messageForCorrectAnswer: "Correct answer. Good job.",
       messageForIncorrectAnswer: "Incorrect answer.",
@@ -101,21 +95,17 @@ export const quiz1 = {
       question:
         "_________ can be done while more than one element needs to be returned from a component.",
       questionType: "text",
-       // if you need to display Picture in Question
+      // if you need to display Picture in Question
       answerSelectionType: "single",
-      answers: [
-        "Abstraction",
-        "Packing",
-        "Insultation",
-        "Wrapping",
-      ],
+      answers: ["Abstraction", "Packing", "Insultation", "Wrapping"],
       correctAnswer: "4",
       messageForCorrectAnswer: "Correct answer. Good job.",
       messageForIncorrectAnswer: "Incorrect answer.",
       point: "20",
     },
-     {
-      question: "Which of the following needs to be updated to achieve dynamic UI updates?",
+    {
+      question:
+        "Which of the following needs to be updated to achieve dynamic UI updates?",
       questionType: "text",
       answerSelectionType: "single",
       answers: ["State", "Props"],
@@ -126,40 +116,30 @@ export const quiz1 = {
         "State is a plain JavaScript object used by React to represent an information about the component's current situation. It's managed in the component (just like any variable declared in a function).",
       point: "20",
     },
-        {
-      question:
-        "In JSX most of the errors can be caught during _________.",
+    {
+      question: "In JSX most of the errors can be caught during _________.",
       questionType: "text",
-       // if you need to display Picture in Question
+      // if you need to display Picture in Question
       answerSelectionType: "single",
-      answers: [
-        "Interpretation",
-        "Execution",
-        "Compilation",
-        "Build",
-      ],
+      answers: ["Interpretation", "Execution", "Compilation", "Build"],
       correctAnswer: "3",
       messageForCorrectAnswer: "Correct answer. Good job.",
       messageForIncorrectAnswer: "Incorrect answer.",
-      explanation:"Compilation error refers to a state when a compiler fails to compile a piece of computer program source code, either due to errors in the code, or, more unusually, due to errors in the compiler itself. A compilation error message often helps programmers debugging the source code.",
+      explanation:
+        "Compilation error refers to a state when a compiler fails to compile a piece of computer program source code, either due to errors in the code, or, more unusually, due to errors in the compiler itself. A compilation error message often helps programmers debugging the source code.",
       point: "20",
     },
-            {
-      question:
-        "What is the smallest building block of ReactJS?",
+    {
+      question: "What is the smallest building block of ReactJS?",
       questionType: "text",
-       // if you need to display Picture in Question
+      // if you need to display Picture in Question
       answerSelectionType: "single",
-      answers: [
-        "none of the options",
-        "props",
-        "elements",
-        "components",
-      ],
+      answers: ["none of the options", "props", "elements", "components"],
       correctAnswer: "3",
       messageForCorrectAnswer: "Correct answer. Good job.",
       messageForIncorrectAnswer: "Incorrect answer.",
-      explanation:"Elements are the smallest building blocks of React apps. An element describes what you want to see on the screen: const element = <h1>Hello, world</h1>; Unlike browser DOM elements, React elements are plain objects, and are cheap to create.",
+      explanation:
+        "Elements are the smallest building blocks of React apps. An element describes what you want to see on the screen: const element = <h1>Hello, world</h1>; Unlike browser DOM elements, React elements are plain objects, and are cheap to create.",
       point: "20",
     },
   ],
